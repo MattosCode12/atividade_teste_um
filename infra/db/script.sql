292d49c200a6728ffa44a30b0d70001c7c57d6c9
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-06-01 11:12:02 -0300

    ADD - Comentários  script.sql

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,22 @@
+-- Cria o banco de dados do sistema
 CREATE DATABASE sistema_simples;
 
+-- Seleciona o banco de dados para utilização
 USE sistema_simples;
 
+--Cria a tabela de usuários
 CREATE TABLE usuario (
+
+    -- Identificador único do usuário
     id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- Nome de usuário para login
     usuario VARCHAR(255) NOT NULL,
+
+    -- Senha do usuário
     senha VARCHAR(255) NOT NULL
 );
 
-INSERT INTO usuario (usuario, senha) VALUE ('admin','123');
+-- Insere um usuário inicial para testes
+INSERT INTO usuario (usuario, senha)
+VALUES ('admin', '123');
