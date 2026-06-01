3a3fd86419c106982526ad887ce1ae4b7496986b
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-06-01 09:44:24 -0300

    ADD - Comentários logout

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,15 @@
 <?php
-    session_start();
-    session_destroy();
-    header("Location: ../index.php");
-    exit();
+
+// Inicia ou recupera a sessão atual
+session_start();
+
+// Remove todos os dados da sessão
+session_destroy();
+
+// Redireciona o usuário para a página inicial
+header("Location: ../index.php");
+
+// Encerra a execução do script
+exit();
+
 ?>
