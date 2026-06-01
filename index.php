b5e1f46c8a98c896ed92104dd5abc117f93fa240
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-06-01 09:37:26 -0300

    ADD - COmentários no index

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <?php
 
+// Importa a conexão com o banco de dados
 include("infra/db/connect.php");
 
+// Verifica se o formulário foi enviado via POST
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    // Recebe os dados digitados pelo usuário
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
-    $sql = "SELECT * FROM users 
-    WHERE username = '$usuario' 
+    // Consulta no banco para verificar usuário e senha
+    $sql = "SELECT * FROM users
+    WHERE username = '$usuario'
     AND password = '$senha'";
 
-    $resultado = $conn -> query($sql);
+    // Executa a consulta
+    $resultado = $conn->query($sql);
 
-    if($resultado -> num_rows > 0){
+    // Verifica se encontrou algum registro
+    if($resultado->num_rows > 0){
+
+        // Cria uma sessão para o usuário autenticado
         $_SESSION["usuario"] = $usuario;
+
+        // Redireciona para a página principal
         header("Location: public/home.php");
         exit();
+
     }else{
+
+        // Mensagem de erro caso o login seja inválido
         $erro = "Usuário ou senha inválidos.";
     }
 }
@@ -31,29 +44,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login com PHP</title>
 </head>
 <body>
-     <?php
+
+    <?php
+    // Importa componente adicional da aplicação
     include("../public/component/table.php");
     ?>
-    <h2>Login com PHP</h2><form method="POST">
-        <label for="usuario">Usuario:</label>
+
+    <h2>Login com PHP</h2>
+
+    <!-- Formulário de autenticação -->
+    <form method="POST">
+
+        <label for="usuario">Usuário:</label>
         <input type="text" name="usuario">
-        <br>
-        <br>
+
+        <br><br>
+
         <label for="senha">Senha:</label>
         <input type="password" name="senha">
-        <br>
-        <br>
+
+        <br><br>
+
         <?php
 
-            if(isset($erro)){
-                echo $erro;
-            }
+        // Exibe mensagem de erro caso exista
+        if(isset($erro)){
+            echo $erro;
+        }
+
         ?>
+
         <button type="submit">Entrar</button>
+
     </form>
-    
 
-
-    
 </body>
 </html>
