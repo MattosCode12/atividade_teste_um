fe776ee6002162d6725928cc3b14c23f8b4f3bcc
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-06-01 10:48:49 -0300

    ADD - Comentário listagem de usuários

--- public/component/table.php
+++ public/component/table.php
@@ -1,7 +1,8 @@
 <hr>
 
-<h4> Usuários Cadastrados</h4>
+<h4>Usuários Cadastrados</h4>
 
+<!-- Tabela para exibição dos usuários -->
 <table border="1" cellpadding="2">
 
     <tr>
@@ -11,22 +12,26 @@
     </tr>
 
     <?php
-    
 
+    // Consulta todos os usuários cadastrados
     $sqlUsuarios = "SELECT * FROM users";
 
-    $resultadoUsuarios = $conn -> query($sqlUsuarios);
+    // Executa a consulta
+    $resultadoUsuarios = $conn->query($sqlUsuarios);
 
+    // Percorre cada registro retornado
     while($linha = $resultadoUsuarios->fetch_assoc()){
+
+        // Exibe os dados na tabela
         echo "<tr>
-        
+
             <td>" . $linha["id"] . "</td>
             <td>" . $linha["username"] . "</td>
             <td>" . $linha["password"] . "</td>
-        
+
         </tr>";
     }
-    
+
     ?>
 
 </table>
