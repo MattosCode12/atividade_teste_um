bb85f8cce04aad1c759f160be10be44b75dbc52f
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-06-01 11:05:04 -0300

    ADD - Comentários connect.php

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,12 +1,16 @@
 <?php
-    session_start();
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db = "sistema_simples";
-    
-    $conn = new mysqli($host,$user,$pass,$db);
+// Inicia ou recupera a sessão do usuário
+session_start();
+
+// Configurações do banco de dados
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "sistema_simples";
+
+// Cria a conexão com o MySQL
+$conn = new mysqli($host, $user, $pass, $db);
 
     // if($conn->connect_error){
     //     die("Erro na conexão");
